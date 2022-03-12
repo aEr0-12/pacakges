a7fe3f71764e18763d79ec6cfe35c3e8b2922a4b
Shubhankar Banerjee <shubhankarbanerjee16@gmail.com>
2022-03-12 20:36:58 +0530

    Script to install all the required packages.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Packages Script
+1. To download the required packages. Just make this file executable and run it.
---
+++ package_install.sh
@@ -0,0 +1,1 @@
+sudo pacman -S xorg xterm xdg-user-dirs pulseaudio pauvcontrol engrampa xed celluloid xfce4-power-manager xfce4-appfinder flameshot lxappearance tint2 picom htop copyq kitty thunar nitrogen obconf xdotool playerctl zsh zsh-highlighting
