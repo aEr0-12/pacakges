e23b8dcce1cd090111dde58f58bfcb3f8d599810
Shubhankar Banerjee <78584638+aero-26@users.noreply.github.com>
2022-04-06 06:25:34 +0530

    Update package_install.sh

--- package_install.sh
+++ package_install.sh
@@ -1,4 +1,5 @@
-sudo pacman -S base-devel xorg xterm xdg-user-dirs pulseaudio pavucontrol engrampa xed celluloid xfce4-power-manager xfce4-appfinder flameshot lxappearance tint2 picom htop copyq kitty thunar gvfs nitrogen obconf xdotool playerctl zsh zsh-syntax-highlighting scrot imagemagick i3lock transmission-gtk tumbler thunar-volman thunar-archive-plugin dunst telegram-desktop code sddm networkmanager nm-connection-editor network-manager-applet bluez blueberry 
+sudo pacman -S --noconfirm base-devel xorg xterm xdg-user-dirs pulseaudio pavucontrol engrampa xed celluloid xfce4-power-manager xfce4-appfinder flameshot lxappearance tint2 picom htop copyq kitty thunar gvfs nitrogen obconf xdotool playerctl zsh zsh-syntax-highlighting scrot imagemagick i3lock transmission-gtk tumbler thunar-volman thunar-archive-plugin dunst telegram-desktop code sddm networkmanager nm-connection-editor network-manager-applet bluez blueberry grub efibootmgr os-prober
+
 systemctl enable NetworkManager
 systemctl enable sddm.service
 systemctl enable bluetooth.service
